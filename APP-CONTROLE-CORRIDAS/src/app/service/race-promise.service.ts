7218b59b10aca1e064267c16b265e34658d6305a
Fernando <setec.solda@gmail.com>
2022-06-11 22:21:10 -0300

    alter

---
+++ APP-CONTROLE-CORRIDAS/src/app/service/race-promise.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Injectable } from '@angular/core';
+import { Corrida } from '../model/corrida';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RacePromiseService {
+  URL = 'http://localhost:3000/races';
+  URL_PT = 'http://localhost:3000/corridas';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private httpClient: HttpClient) {}
+
+  getByRace(corridas: string): Promise<Corrida[]> {
+    return this.httpClient.get<Corrida[]>(`${this.URL_PT}/${corridas}`).toPromise();
+  }
+
+  saveRace(corrida: Corrida): Promise<Corrida> {
+    return this.httpClient
+      .post<Corrida>(this.URL, JSON.stringify(corrida), this.httpOptions)
+      .toPromise();
+  }
+
+  patchRace(corrida: Corrida): Promise<Corrida> {
+    return this.httpClient
+      .patch<Corrida>(
+        `${this.URL}/${corrida.corridas}`,
+        JSON.stringify(corrida),
+        this.httpOptions
+      )
+      .toPromise();
+  }
+
+  updateRace(corrida: Corrida): Promise<Corrida> {
+    return this.httpClient
+      .put<Corrida>(
+        `${this.URL}/${corrida.corridas}`,
+        JSON.stringify(corrida),
+        this.httpOptions
+      )
+      .toPromise();
+  }
+}
